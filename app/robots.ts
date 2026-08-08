dc532c39264d9d1e2931205ec6984c280930e029
zenoK80 <stcfs80@gmail.com>
2026-08-08 12:37:22 +0900

    fix: generate robots metadata as static output

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 const SITE_URL = 'https://zeno.it.kr'
 
 export default function robots(): MetadataRoute.Robots {
