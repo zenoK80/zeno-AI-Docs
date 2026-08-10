2e1ac1f4132dcb377805bb4bcda6dfc66b43f314
zenoK80 <stcfs80@gmail.com>
2026-08-10 10:23:09 +0900

    feat: publish SQLD lessons 06-10

--- content/자격증/SQLD/_meta.js
+++ content/자격증/SQLD/_meta.js
@@ -9,6 +9,16 @@ const meta = {
   '04_data-model-overview-review': '04-복습. 데이터 모델의 이해',
   '05_entity': '05. 엔터티',
   '05_entity-review': '05-복습. 엔터티',
+  '06_attribute': '06. 속성',
+  '06_attribute-review': '06-복습. 속성',
+  '07_relationship': '07. 관계',
+  '07_relationship-review': '07-복습. 관계',
+  '08_identifier': '08. 식별자',
+  '08_identifier-review': '08-복습. 식별자',
+  '09_normalization': '09. 정규화',
+  '09_normalization-review': '09-복습. 정규화',
+  '10_relationship-and-join': '10. 관계와 조인의 이해',
+  '10_relationship-and-join-review': '10-복습. 관계와 조인의 이해',
 }
 
 export default meta
