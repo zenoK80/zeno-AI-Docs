0ae26dd1233d833539586e2c69003f1fef66183e
zenoK80 <stcfs80@gmail.com>
2026-08-24 03:12:39 +0900

    docs: complete 독학사 머신러닝 10-14 and 데이터베이스 09-16 with meta files

---
+++ content/독학사/database/_meta.js
@@ -0,0 +1,19 @@
+const meta = {
+  '01_database-overview': '01. 데이터베이스 개요',
+  '02_data-modeling': '02. 데이터 모델링',
+  '03_relational-model': '03. 관계형 모델',
+  '04_relational-algebra': '04. 관계 대수',
+  '05_sql-basics': '05. SQL 기초',
+  '06_sql-advanced': '06. SQL 고급',
+  '07_normalization': '07. 정규화',
+  '08_transaction': '08. 트랜잭션',
+  '09_recovery': '09. 회복 기법',
+  '10_index': '10. 인덱스',
+  '11_query-optimization': '11. 쿼리 최적화',
+  '12_distributed-database': '12. 분산 데이터베이스',
+  '13_nosql': '13. NoSQL',
+  '14_bigdata-and-dw': '14. 빅데이터와 데이터 웨어하우스',
+  '15_database-security': '15. 데이터베이스 보안',
+  '16_db-practice': '16. 데이터베이스 예상기출문제',
+}
+export default meta
