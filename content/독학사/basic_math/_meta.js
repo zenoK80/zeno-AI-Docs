1939e8a04a2b1370f94ffb154074adcb274cc481
zenoK80 <stcfs80@gmail.com>
2026-08-24 10:20:03 +0900

    docs: add 기초통계학/일반수학 03-10 chapters and fix junction diamond size

--- content/독학사/basic_math/_meta.js
+++ content/독학사/basic_math/_meta.js
@@ -1,6 +1,14 @@
 const meta = {
   '01_sets-and-operations': '01. 집합과 연산',
   '02_functions': '02. 함수, 합성함수와 역함수',
+  '03_sequences-and-series': '03. 수열과 급수',
+  '04_limits': '04. 극한과 연속',
+  '05_differentiation': '05. 미분',
+  '06_integration': '06. 적분',
+  '07_matrices': '07. 행렬',
+  '08_vectors': '08. 벡터',
+  '09_complex-numbers': '09. 복소수',
+  '10_combinatorics': '10. 경우의 수와 확률',
 }
 
 export default meta
--- content/독학사/basic_statics/_meta.js
+++ content/독학사/basic_statics/_meta.js
@@ -1,6 +1,14 @@
 const meta = {
   '01_population-sample-and-data-types': '01. 모집단·표본과 자료의 종류',
   '02_frequency-tables-and-graphs': '02. 도수분포표와 그래프',
+  '03_central-tendency-and-dispersion': '03. 대표값과 산포도',
+  '04_probability-basics': '04. 확률의 기초',
+  '05_conditional-probability': '05. 조건부 확률과 독립',
+  '06_discrete-probability-distribution': '06. 이산확률분포',
+  '07_normal-distribution': '07. 정규분포',
+  '08_sampling-and-estimation': '08. 표본분포와 추정',
+  '09_hypothesis-testing': '09. 가설검정',
+  '10_correlation-and-regression': '10. 상관분석과 회귀분석',
 }
 
 export default meta
