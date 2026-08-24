f50b12ba7dceaa4c6bbee14ccf73ae74b73bc115
zenoK80 <stcfs80@gmail.com>
2026-08-25 01:36:23 +0900

    docs: 독학사 기초통계학 기술통계 5편 전면 재작성 (모집단/표본, 도수분포표, 평균/중앙값/최빈값, 산포, 상대적위치)

--- content/독학사/basic_statics/_meta.js
+++ content/독학사/basic_statics/_meta.js
@@ -3,6 +3,11 @@ const meta = {
   '02_data-tables-and-graphs': '02. 표와 그래프 읽기',
   '03_sigma-notation': '03. 시그마(Σ) 표기법',
   '04_functions-and-coordinates': '04. 함수와 좌표 기초',
+  '05_population-sample-and-data-types': '05. 모집단, 표본과 자료의 종류',
+  '06_frequency-tables-and-graphs': '06. 도수분포표와 그래프',
+  '07_measures-of-center': '07. 평균, 중앙값, 최빈값',
+  '08_spread-and-variability': '08. 범위, 분산, 표준편차',
+  '09_relative-position': '09. 백분위수, Z점수, 사분위수',
 }
 
 export default meta
