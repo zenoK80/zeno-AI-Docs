b34c060902cdd545cde528941c42989eab13b107
zenoK80 <stcfs80@gmail.com>
2026-08-25 09:04:01 +0900

    docs: 독학사 운영체제 본문 2편 작성 (운영체제개요, 프로세스와스레드)

--- content/독학사/os/_meta.js
+++ content/독학사/os/_meta.js
@@ -2,6 +2,8 @@ const meta = {
   '01_computer-hardware-basics': '01. 컴퓨터 하드웨어 기초',
   '02_binary-and-memory-address': '02. 이진수와 메모리 주소',
   '03_program-execution': '03. 프로그램 실행 흐름',
+  '04_os-overview': '04. 운영체제 개요',
+  '05_process-and-thread': '05. 프로세스와 스레드',
 }
 
 export default meta
