37df132ef5b73b7e6f4c28f49d1e7f5bd8baf895
zenoK80 <stcfs80@gmail.com>
2026-08-25 09:12:05 +0900

    docs: 독학사 운영체제 본문 3편 작성 (CPU스케줄링, 프로세스동기화, 교착상태) + 표 셀 물결표 빌드에러 버그 수정 및 문서화

--- content/독학사/os/_meta.js
+++ content/독학사/os/_meta.js
@@ -4,6 +4,9 @@ const meta = {
   '03_program-execution': '03. 프로그램 실행 흐름',
   '04_os-overview': '04. 운영체제 개요',
   '05_process-and-thread': '05. 프로세스와 스레드',
+  '06_cpu-scheduling': '06. CPU 스케줄링',
+  '07_process-synchronization': '07. 프로세스 동기화',
+  '08_deadlock': '08. 교착상태',
 }
 
 export default meta
