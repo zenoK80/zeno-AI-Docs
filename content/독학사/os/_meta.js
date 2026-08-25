82fcf1d6839b2cbd3a79646b284d6ff87ae6183a
zenoK80 <stcfs80@gmail.com>
2026-08-25 09:17:28 +0900

    docs: 독학사 운영체제 본문 3편 작성 (메모리관리, 가상메모리, 파일관리)

--- content/독학사/os/_meta.js
+++ content/독학사/os/_meta.js
@@ -7,6 +7,9 @@ const meta = {
   '06_cpu-scheduling': '06. CPU 스케줄링',
   '07_process-synchronization': '07. 프로세스 동기화',
   '08_deadlock': '08. 교착상태',
+  '09_memory-management': '09. 메모리 관리',
+  '10_virtual-memory': '10. 가상 메모리',
+  '11_file-management': '11. 파일 관리',
 }
 
 export default meta
